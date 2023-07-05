8e42d292b3e156462f4d28875ec7fa541ad0061d
Thomassanz23 <thomas.sanz1998@gmail.com>
2023-07-04 21:16:23 -0300

    cursa de ingreso

--- ejercicios/01_entrada_salida/ejercicio_02/app.py
+++ ejercicios/01_entrada_salida/ejercicio_02/app.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Thomas 
+apellido: Sanchez
 ---
 Ejercicio: entrada_salida_02
 ---
@@ -28,8 +28,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
-        
+       Nombre = prompt (title="Pregunta",prompt="¿Cual es su nombre?")
+       alert(title="INFO", message=Nombre)
         
     
 if __name__ == "__main__":
--- ejercicios/01_entrada_salida/ejercicio_03/app.py
+++ ejercicios/01_entrada_salida/ejercicio_03/app.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Thomas 
+apellido: Sanchez
 ---
 Ejercicio: entrada_salida_03
 ---
@@ -34,8 +34,8 @@ class App(customtkinter.CTk):
 
 
     def btn_mostrar_on_click(self):
-        pass
-                
+        contenido = self.txt_nombre.get()
+        alert(title="EJ 03", message=contenido)     
     
 if __name__ == "__main__":
     app = App()
